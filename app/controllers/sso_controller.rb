bd3fb1d82959dbe959dabed1572339dd059ef065
omsingh-rgb <60109562+omsingh-rgb@users.noreply.github.com>
2023-06-08 20:21:21 +0100

    Final push

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -10,8 +10,10 @@ class SsoController < ApplicationController
 
   def event_page
     @event = Event.where(id: params[:eid]).first
-    @registered = CadetRegistration.where(event: @event)
-    @interested = CadetInterest.where(event: @event)
+    @registered = CadetRegistration.joins(:cadet).where(event: @event).where(cadet: {squadron_id: 1})
+    @interested = CadetInterest.joins(:cadet).where(event: @event)
+                               .where(cadet: {squadron_id: 1})
+                               .where.not(cadet_id: @registered.select(:cadet_id)).distinct
     @date_range = @event.age_range.before..@event.age_range.after
     @available  = Cadet.where(squadron_id: 1)
                    .where(dob: @date_range)
@@ -24,5 +26,58 @@ class SsoController < ApplicationController
   def event_page_bad
     @event = Event.where(id: params[:eid]).first
   end
+
+  def unregister_cadet
+    CadetRegistration.where(event_id: params[:eid], cadet_id: params[:cid]).first.delete
+  end
+
+  def uninterested_cadet
+    CadetInterest.where(event_id: params[:eid], cadet_id: params[:cid]).first.delete
+  end
+
+  def register_cadet
+    CadetRegistration.create(cadet_id: params[:cid],
+                             event_id: params[:eid])
+  end
+
+  def interested_cadet
+    CadetInterest.create(cadet_id: params[:cid],
+                             event_id: params[:eid])
+  end
+
+  def cadets
+    @cadets = Cadet.where(squadron_id: 1).order(created_at: :desc)
+  end
+
+  def cadet_create
+
+    Cadet.create(squadron_id: 1,
+                 firstname: params[:firstname],
+                 lastname: params[:lastname],
+                 dob: params[:dob],
+                 cid: params[:cid],
+                 rank: params[:rank],
+                 competing_category: params[:competing_category])
+
+  end
+
+  def cadet_update
+    c = Cadet.where(id: params[:cid]).first
+
+    c.update(firstname: params[:firstname],
+                 lastname: params[:lastname],
+                 dob: params[:dob],
+                 cid: params[:caid],
+                 rank: params[:rank],
+                 competing_category: params[:competing_category])
+  end
+
+  def cadet_delete
+    c = Cadet.where(id: params[:cid]).first
+    CadetRegistration.where(cadet: c).delete_all
+    CadetInterest.where(cadet: c).delete_all
+    c.delete
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,15 @@ Rails.application.routes.draw do
   get '/sso/:compid/events', to: 'sso#competition_page'
   get '/sso/event/:eid', to: 'sso#event_page'
   get '/sso/event-bad/:eid', to: 'sso#event_page_bad'
+  get 'sso/cadets', to: 'sso#cadets'
+
+  post '/sso/unregister-cadet/:eid/:cid', to: "sso#unregister_cadet"
+  post '/sso/uninterested-cadet/:eid/:cid', to: "sso#uninterested_cadet"
+  post '/sso/register-cadet/:eid/:cid', to: "sso#register_cadet"
+  post '/sso/interested-cadet/:eid/:cid', to: "sso#interested_cadet"
+  post '/sso/cadet-create/', to: 'sso#cadet_create'
+  post '/sso/cadet-update/:cid', to: 'sso#cadet_update'
+  post '/sso/cadet-delete/:cid', to: 'sso#cadet_delete'
+
 
 end
